780823dc972f2034f3b22a27ade11467e96ace44
Euber Santos <euber.olavo.bezerra@gmail.com>
2022-09-07 13:02:17 -0300

    modificando html

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Ola mundo</h1>
+      <div className="imagem">
+        <img></img>
+      </div>
+      <div className="formbox">
+        <h3>Acesse a sua conta</h3>
+        <div>
+          <label>E-mail</label>
+          <input></input>
+          <label>Senha</label>
+          <input></input>
+          <input type="checkbox"></input>
+        </div>
+        <button></button>
+        <link>Esqueceu a sua senha?</link>
+        <link>
+          Não tem uma conta ainda<p>Registre-se</p>
+        </link>
+      </div>
     </div>
   );
 }
